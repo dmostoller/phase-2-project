b011409dc8398b73626c42b5b3bedb47f5b2b3e9
Tim McCann <timothysmccann@gmail.com>
2024-01-22 18:11:53 -0500

    details and comments started

---
+++ src/components/PaintingDetail.js
@@ -0,0 +1,40 @@
+import React, {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
+import {Link} from "react-router-dom";
+import CommentsList from "./CommentsList";
+
+function PaintingDetail(){
+    const [painting, setPainting] = useState({})
+    const {id} = useParams();
+    
+    useEffect(() => {
+        fetch(`http://localhost:3004/paintings/${id}`)
+        .then((res) => res.json())
+        .then((painting) => setPainting(painting))
+    }, []);
+
+    if (!painting) {return <div>Loading...</div>}
+
+    return (
+        <div className="ui horizontal box">
+            <div className="image">
+                <img src={painting.image} alt={painting.title}></img>
+            </div>
+            <div className="content">
+                <a className="header">{painting.title}</a>
+                <div className="description">{painting.materials}</div>
+                <div className="description">{painting.width}" x {painting.height}"</div>
+            
+                <div className="description">
+                    {painting.sold ? "SOLD" : <a>{painting.price}</a>}
+                </div>
+                <div><CommentsList /></div>
+                <div style={{paddingBottom: "5px", float: "right"}}> 
+                    <Link to="/paintings" className="ui button small teal" >Back</Link>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default PaintingDetail
